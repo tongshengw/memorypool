81efa2e97527d18a3a030c9e61e774fdf9228f5d
Tongsheng Wu <tongshengwu0@gmail.com>
2025-05-16 02:27:04 -0400

    added asserts, working on some list functions

--- cpu-concept/poolalloc.c
+++ cpu-concept/poolalloc.c
@@ -9,8 +9,43 @@ static char memPool[MEM_POOL_SIZE];
 static BlockHeader *freeList;
 static BlockHeader *usedList;
 
+// NOTE:: Debug functions
+static int debugListSize(BlockHeader *head) {
+    int size = 0;
+    BlockHeader *current = head;
+    while (current != NULL) {
+        size++;
+        current = current->next;
+    }
+    return size;
+}
+
+static void assertListValid(BlockHeader *head) {
+    BlockHeader *current = head;
+    while (current != NULL) {
+        if (current->next != NULL) {
+            assert(current->next->prev == current);
+        }
+        if (current->prev != NULL) {
+            assert(current->prev->next == current);
+        } else {
+            assert(current == head);
+        }
+        current = current->next;
+    }
+}
+
+static void assertFreeListSorted(BlockHeader *head) {
+    while (head && head->next) {
+        assert(head->size <= head->next->size);
+        assert(head->next->prev == head);
+        head = head->next;
+    }
+}
+
 // NOTE: input pointer to data, will automatically calculate BlockHeader pointer
 // address by subtracting sizeof(BlockHeader)
+// FIXME: double pointer head
 static BlockHeader *listLinearFind(BlockHeader *head, void *target) {
     BlockHeader *start = head;
     while (start != NULL) {
@@ -30,14 +65,21 @@ static void listRemove(BlockHeader *target) {
     back->next = front;
 }
 
-static void listPrepend(BlockHeader *head, BlockHeader *added) {
-    head->prev = added;
-    added->next = head;
+static void listPrepend(BlockHeader **head, BlockHeader *added) {
+    if (*head == NULL) {
+        *head = added;
+        added->prev = NULL;
+        added->next = NULL;
+        return;
+    }
+    (*head)->prev = added;
+    added->next = *head;
     added->prev = NULL;
-    head = added;
+    *head = added;
 }
 
 // NOTE: only use on lists with length greater than or equal 2
+// FIXME: double pointer head
 static void listSwapHeadSort(BlockHeader *head) {
     assert(head->next->next != NULL);
     BlockHeader *front = head->next->next;
@@ -65,44 +107,62 @@ static void listSwapHeadSort(BlockHeader *head) {
     head = tmp;
 }
 
-unsigned long max(unsigned long a, unsigned long b) { return a > b ? a : b; }
+static unsigned long max(unsigned long a, unsigned long b) {
+    return a > b ? a : b;
+}
 
 void poolinit() {
     BlockHeader *header = (BlockHeader *)memPool;
     // create a block that fills entire pool
     header->size = MEM_POOL_SIZE - sizeof(BlockHeader);
-    listPrepend(freeList, header);
+    listPrepend(&freeList, header);
 }
 
 void *poolmalloc(unsigned long size) {
     // all pointer arithmetic is done on char* for clarity
+    // TODO: handle edge case where last alloc takes space of last header
     if (freeList->size < size) {
         return NULL;
     }
+
+    int initFreeListSize = debugListSize(freeList);
+    int initUsedListSize = debugListSize(usedList);
 
     unsigned long oldBlockSize = freeList->size;
     BlockHeader *newAllocatedHeader = freeList;
     unsigned long sizeToAllocate = max(size, 8);
     newAllocatedHeader->size = sizeToAllocate;
     listRemove(freeList);
-    listPrepend(usedList, newAllocatedHeader);
-
+    listPrepend(&usedList, newAllocatedHeader);
     BlockHeader *newFreeHeader =
         (BlockHeader *)((char *)newAllocatedHeader +
                         (sizeof(BlockHeader) + sizeToAllocate));
     newFreeHeader->size = oldBlockSize - (sizeToAllocate + sizeof(BlockHeader));
-    listPrepend(freeList, newFreeHeader);
-
+    listPrepend(&freeList, newFreeHeader);
     listSwapHeadSort(freeList);
-
     void *res = (char *)newAllocatedHeader + (sizeof(BlockHeader));
+
+    assertListValid(freeList);
+    assertListValid(usedList);
+    assert(debugListSize(freeList) == initFreeListSize);
+    assert(debugListSize(usedList) == initUsedListSize + 1);
+    assertFreeListSorted(freeList);
     return res;
 }
 
 void poolfree(void *ptr) {
+    int initFreeListSize = debugListSize(freeList);
+    int initUsedListSize = debugListSize(usedList);
+
     char *poolPtr = (char *)ptr;
     BlockHeader *freedHeader = (BlockHeader *)(poolPtr - sizeof(BlockHeader));
     BlockHeader *freedUsedListPtr = listLinearFind(usedList, freedHeader);
     listRemove(freedUsedListPtr);
-    listPrepend(freeList, freedHeader);
+    listPrepend(&freeList, freedHeader);
+
+    assertListValid(freeList);
+    assertListValid(usedList);
+    assert(debugListSize(freeList) == initFreeListSize + 1);
+    assert(debugListSize(usedList) == initUsedListSize - 1);
+    assertFreeListSorted(freeList);
 }
