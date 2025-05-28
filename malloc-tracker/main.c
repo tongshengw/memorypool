950905d26ea7661fc37d32d2aad25543e2097266
Tongsheng Wu <tongshengwu0@gmail.com>
2025-05-28 14:19:11 -0400

    malloc tracker correctly tracks peak usage

--- malloc-tracker/main.c
+++ malloc-tracker/main.c
@@ -4,7 +4,10 @@
 int main(void) {
     int *ptr1 = trackedmalloc(100);
     int *ptr2 = trackedmalloc(200);
+    trackedfree(ptr1);
     int *ptr3 = trackedmalloc(300);
+    int *ptr4 = trackedmalloc(400);
+    trackedfree(ptr2);
     
-    printf("Total bytes allocated: %lu\n", get_total_bytes());
+    printf("Peak memory usage: %lu\n", get_peak_usage());
 }
--- malloc-tracker/trackedmalloc.c
+++ malloc-tracker/trackedmalloc.c
@@ -1,15 +1,61 @@
 #include "trackedmalloc.h"
 #include <assert.h>
+#include <stdbool.h>
 
-unsigned long totalBytes = 0;
+// have to use custom tracking because malloc_usable_size() not available in
+// macos
+typedef struct {
+    size_t size;
+    void *ptr;
+} AllocatedPtr;
+
+static size_t curUsage = 0;
+static size_t maxUsage = 0;
+
+static AllocatedPtr *allocatedPtrs;
+static size_t allcoatedPtrsSize = 0;
+static size_t allocatedPtrsCapacity = 0;
+
+static void allocatedPtrsAdd(void *ptr, size_t size) {
+    if (allocatedPtrsCapacity == 0) {
+        allocatedPtrsCapacity = 16;
+        allocatedPtrs = malloc(allocatedPtrsCapacity * sizeof(AllocatedPtr));
+        assert(allocatedPtrs != NULL);
+    } else if (allcoatedPtrsSize >= allocatedPtrsCapacity) {
+        allocatedPtrsCapacity *= 2;
+        AllocatedPtr *newAllocatedPtrs = realloc(
+            allocatedPtrs, allocatedPtrsCapacity * sizeof(AllocatedPtr));
+        return;
+    }
+    allocatedPtrs[allcoatedPtrsSize].ptr = ptr;
+    allocatedPtrs[allcoatedPtrsSize].size = size;
+    allcoatedPtrsSize++;
+    curUsage += size;
+    maxUsage = (curUsage > maxUsage) ? curUsage : maxUsage;
+}
+
+static void allocatedPtrsRemove(void *ptr) {
+    for (size_t i = 0; i < allcoatedPtrsSize; i++) {
+        if (allocatedPtrs[i].ptr == ptr) {
+            curUsage -= allocatedPtrs[i].size;
+            allocatedPtrs[i] = allocatedPtrs[allcoatedPtrsSize - 1];
+            allcoatedPtrsSize--;
+            return;
+        }
+    }
+    assert(false);
+}
 
 void *trackedmalloc(size_t size) {
-    totalBytes += size;
     void *ptr = malloc(size);
+    allocatedPtrsAdd(ptr, size);
     assert(ptr != NULL);
     return ptr;
 }
 
-unsigned long get_total_bytes() {
-    return totalBytes;
+void trackedfree(void *ptr) {
+    allocatedPtrsRemove(ptr);
+    free(ptr);
 }
+
+size_t get_peak_usage() { return maxUsage; }
