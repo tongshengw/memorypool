5d2d6605b520fe3faf7b657e22dbdf1a8a015a21
Tongsheng Wu <tongshengwu0@gmail.com>
2025-05-30 15:22:04 -0400

    cmake working

--- linalg/main.c
+++ linalg/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "linalg.h"
+#include <linalg.h>
 
 // test vvdot
 void test_vvdot()
--- linalg/malloc-macro.h
+++ linalg/malloc-macro.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #ifdef USE_POOL
+  #include <poolalloc.h>
   #define malloc(size) poolmalloc(size)
   #define free(ptr)    poolfree(ptr)
 #endif
