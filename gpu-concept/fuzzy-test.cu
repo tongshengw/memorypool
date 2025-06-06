0312435a7204925640930f01f0b1d635cfb1b9e6
Tongsheng Wu <tongshengwu0@gmail.com>
2025-06-06 18:58:21 -0400

    fuzzy test for gpu

---
+++ gpu-concept/fuzzy-test.cu
@@ -0,0 +1,81 @@
+// fuzzy testing
+/*
+  
+*/
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include <unordered_set>
+#include <assert.h>
+
+#include "poolalloc.cuh"
+
+#define NUM_THREADS 128
+#define OPS_PER_THREAD 100
+
+struct TestOperation {
+    bool isAlloc;
+    unsigned long numBytes;
+    int corrospondingAlloc;
+};
+
+void generateRandomOperations(TestOperation *ops) {
+    for (int i = 0; i < NUM_THREADS; i++) {
+        std::unordered_set<size_t> allocationIndices;
+        size_t startingInd = i * OPS_PER_THREAD;
+        for (int j = 0; j < OPS_PER_THREAD; j++) {
+            if (allocationIndices.size() == 0) {
+                ops[startingInd + j].isAlloc = true;
+                ops[startingInd + j].numBytes = 4 * sizeof(int);
+                allocationIndices.insert(startingInd + j);
+            } else if (rand() % 2 == 0) {
+                ops[startingInd + j].isAlloc = true;
+                ops[startingInd + j].numBytes = 4 * sizeof(int);
+                ops[startingInd + j].corrospondingAlloc = -1;
+                allocationIndices.insert(startingInd + j);
+            } else {
+                ops[startingInd + j].isAlloc = false;
+                auto it = allocationIndices.begin();
+                std::advance(it, rand() % allocationIndices.size());
+                ops[startingInd + j].corrospondingAlloc = *it;
+                allocationIndices.erase(it);
+            }
+        }
+    }
+}
+
+__global__ void runTests(TestOperation *ops, void *poolMemoryBlock) {
+    unsigned int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    poolinit(poolMemoryBlock, idx);
+    
+    void *allocatedPtrs[OPS_PER_THREAD];
+    
+    for (unsigned int i = 0; i < OPS_PER_THREAD; i++) {
+        unsigned int opIndex = idx * OPS_PER_THREAD + i;
+        if (ops[opIndex].isAlloc) {
+            void *ptr = poolmalloc(ops[opIndex].numBytes);
+            allocatedPtrs[i] = ptr;
+            for (unsigned long j = 0; j < ops[opIndex].numBytes; j++) {
+                ((char*)ptr)[j] = 'a';
+            }
+            printf("Thread %d: Allocated %lu bytes at %p\n", idx, ops[opIndex].numBytes, ptr);
+        } else {
+            char *ptrToFree = (char*)allocatedPtrs[ops[opIndex].corrospondingAlloc];
+            for (unsigned long j = 0; j < ops[opIndex].numBytes; j++) {
+                assert(ptrToFree[j] == 'a');
+            }
+            poolfree(ptrToFree);
+            printf("Thread %d: Freed allocation at index %d\n", idx, ops[opIndex].corrospondingAlloc);
+        }
+    }
+}
+
+int main() {
+    TestOperation ops[NUM_THREADS * OPS_PER_THREAD];
+    generateRandomOperations(ops);
+    
+    TestOperation *d_ops;
+    cudaMalloc(&d_ops, sizeof(TestOperation) * NUM_THREADS * OPS_PER_THREAD);
+    cudaMemcpy(&d_ops, ops, sizeof(TestOperation) * NUM_THREADS * OPS_PER_THREAD, cudaMemcpyHostToDevice);
+
+    runTests<<<NUM_THREADS, 1>>>(d_ops, allocatePools(NUM_THREADS * OPS_PER_THREAD));
+}
