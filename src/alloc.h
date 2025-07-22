b79c6da861a90aaedb03f8c8e143b91a60dc1ec1
Tongsheng Wu <tongshengwu0@gmail.com>
2025-07-22 16:14:12 -0400

    add calloc header

--- src/alloc.h
+++ src/alloc.h
@@ -8,9 +8,15 @@
   #else
     #include <memorypool/cpu/poolalloc.h>  
   #endif
+
+  #define swappablecalloc(count, size) poolcalloc(count, size)
+
   #define swappablemalloc(size) poolmalloc(size)
   #define swappablefree(ptr)    poolfree(ptr)
+
 #else
+  #define swappablecalloc(count, size) calloc(count, size)
+
   #define swappablemalloc(size) malloc(size)
   #define swappablefree(ptr)    free(ptr)
 #endif
